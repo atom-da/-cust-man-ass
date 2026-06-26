0339ead21cd1204f7025de81391ffe9b9906f3a5
Dhana <dhanamoorthida@gmail.com>
2026-06-26 17:16:23 +0530

    git pages push fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
-    base: "/-cust-man-ass/",
+    base: "/cust-man-ass/",
 })
-=======
-  base: "/cust-man-ass/",
-})
->>>>>>> f90a77d24b3285423c3e69ccd13bd36e8bb16ce5
